48e479cea0f8f5dea80b23374e889a7859de3de2
bhajko <bhajko@gmail.com>
2019-09-21 20:06:25 +0200

    carousel added to homepage

---
+++ src/components/carousel/carousel.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+
+export default () => (
+  <Carousel autoPlay infiniteLoop showThumbs={false} showStatus={false}>
+    <div>
+      <img src="https://i.ibb.co/YbxdHtk/snkrhdz-nike.png" alt="snkrhdz-nike" />
+    </div>
+    <div>
+      <img src="https://i.ibb.co/mXGyH66/custom-1.png" alt="snkrhdz-hoodie" />
+    </div>
+    <div>
+      <img
+        src="https://i.ibb.co/MB1C6vL/snkrhdz-adidas.png"
+        alt="snkrhdz-adidas"
+      />
+    </div>
+  </Carousel>
+);
--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Directory from "../../components/directory/directory";
 import "./homepage.scss";
+import Carousel from "../../components/carousel/carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const HomePage = () => {
   return (
-    <div className="homepage">
-      <Directory />
-    </div>
+    <>
+      <Carousel />
+      <div className="homepage">
+        <Directory />
+      </div>
+    </>
   );
 };
 
