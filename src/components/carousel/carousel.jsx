2f2e1e754f59b5eceb8643dc9b72a4c8b3e438cd
bhajko <bhajko@gmail.com>
2019-09-21 20:15:37 +0200

    carousel pic 1 replaced

--- src/components/carousel/carousel.jsx
+++ src/components/carousel/carousel.jsx
@@ -4,7 +4,7 @@ import { Carousel } from "react-responsive-carousel";
 export default () => (
   <Carousel autoPlay infiniteLoop showThumbs={false} showStatus={false}>
     <div>
-      <img src="https://i.ibb.co/YbxdHtk/snkrhdz-nike.png" alt="snkrhdz-nike" />
+      <img src="https://i.ibb.co/Rb1YSw7/snkrhdz-nike.png" alt="snkrhdz-nike" />
     </div>
     <div>
       <img src="https://i.ibb.co/mXGyH66/custom-1.png" alt="snkrhdz-hoodie" />
