cc609789c9e484ac983cab96756e23bb02dc3d89
bhajko <bhajko@gmail.com>
2019-09-28 11:05:49 +0200

    styled-components added, homepage converted

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import Directory from "../../components/directory/directory";
-import "./homepage.scss";
 import Carousel from "../../components/carousel/carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { HomePageContainer } from "./homepage.styled";
 
 const HomePage = () => {
   return (
     <>
       <Carousel />
-      <div className="homepage">
+      <HomePageContainer>
         <Directory />
-      </div>
+      </HomePageContainer>
     </>
   );
 };
