41a8c8bc17ae5178558e47a5c0c51ccbdff5338c
bhajko <bhajko@gmail.com>
2019-10-06 12:02:28 +0200

    carousel text added

--- src/components/carousel/carousel.jsx
+++ src/components/carousel/carousel.jsx
@@ -1,19 +1,41 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
+import "./carousel.scss";
 
 export default () => (
   <Carousel autoPlay infiniteLoop showThumbs={false} showStatus={false}>
-    <div>
+    <div className="slide-text">
       <img src="https://i.ibb.co/Rb1YSw7/snkrhdz-nike.png" alt="snkrhdz-nike" />
+      <div className="text">
+        <h1>AIR MAX 270 SE</h1>
+        <p>
+          Boasting the largest-ever Wmns Max <br />
+          air heel unitcreated by Nike
+        </p>
+      </div>
     </div>
-    <div>
+    <div className="slide-text reverse">
       <img src="https://i.ibb.co/mXGyH66/custom-1.png" alt="snkrhdz-hoodie" />
+      <div className="text">
+        <h1>ADIDAS TREFOIL</h1>
+        <p>
+          Shop the latest selection of <br />
+          Adidas trefoil hoodies
+        </p>
+      </div>
     </div>
-    <div>
+    <div className="slide-text reverse">
       <img
         src="https://i.ibb.co/MB1C6vL/snkrhdz-adidas.png"
         alt="snkrhdz-adidas"
       />
+      <div className="text">
+        <h1>NITE JOGGER</h1>
+        <p>
+          Inspired by a sneaker of the <br />
+          Same name from 1979.
+        </p>
+      </div>
     </div>
   </Carousel>
 );
