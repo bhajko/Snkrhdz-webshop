9642654092ab4a039667252cca84cbb8b8c5d678
bhajko <bhajko@gmail.com>
2019-10-19 19:38:15 +0200

    caps pics added

--- src/redux/shop/shop.data.js
+++ src/redux/shop/shop.data.js
@@ -12,51 +12,57 @@ const SHOP_DATA = {
       },
       {
         id: 2,
-        name: "Blue Beanie",
-        imageUrl: "https://i.ibb.co/ypkgK0X/blue-beanie.png",
-        price: 18
+        name: "NY Yankees Navy",
+        imageUrl: "https://i.ibb.co/kMMMCD5/yankees-navy.png",
+        price: 30
       },
       {
         id: 3,
-        name: "Brown Cowboy",
-        imageUrl: "https://i.ibb.co/QdJwgmp/brown-cowboy.png",
+        name: "Vans Black",
+        imageUrl: "https://i.ibb.co/9yv37h1/vans-black.png",
         price: 35
       },
       {
         id: 4,
-        name: "Grey Brim",
-        imageUrl: "https://i.ibb.co/RjBLWxB/grey-brim.png",
+        name: "Nixon Lightblue",
+        imageUrl: "https://i.ibb.co/GMzZCv5/nixon-lightblue.png",
         price: 25
       },
       {
         id: 5,
-        name: "Green Beanie",
-        imageUrl: "https://i.ibb.co/YTjW3vF/green-beanie.png",
-        price: 18
+        name: "Jordan Pro Red",
+        imageUrl: "https://i.ibb.co/0JF7mnJ/jordan-red.png",
+        price: 40
       },
       {
         id: 6,
-        name: "Palm Tree Cap",
-        imageUrl: "https://i.ibb.co/rKBDvJX/palm-tree-cap.png",
-        price: 14
+        name: "Adidas Navy",
+        imageUrl: "https://i.ibb.co/zJqrVq4/adidas-navy.png",
+        price: 25
       },
       {
         id: 7,
-        name: "Red Beanie",
-        imageUrl: "https://i.ibb.co/bLB646Z/red-beanie.png",
-        price: 18
+        name: "LA Dodgers Olive",
+        imageUrl: "https://i.ibb.co/VJ60W92/la-olive.png",
+        price: 30
       },
       {
         id: 8,
-        name: "Wolf Cap",
-        imageUrl: "https://i.ibb.co/1f2nWMM/wolf-cap.png",
-        price: 14
+        name: "Chicago Bulls Grey",
+        imageUrl: "https://i.ibb.co/PT8BBd4/bulls-grey.png",
+        price: 30
       },
       {
         id: 9,
-        name: "Blue Snapback",
-        imageUrl: "https://i.ibb.co/X2VJP2W/blue-snapback.png",
-        price: 16
+        name: "LA Lakers Purple",
+        imageUrl: "https://i.ibb.co/GT7F3yC/lakers-purple.png",
+        price: 28
+      },
+      {
+        id: 10,
+        name: "NY Giants Blue",
+        imageUrl: "https://i.ibb.co/gzJBVJ6/giants.png",
+        price: 33
       }
     ]
   },
@@ -66,61 +72,61 @@ const SHOP_DATA = {
     routeName: "hoodies",
     items: [
       {
-        id: 10,
+        id: 11,
         name: "New Balance Orange",
         imageUrl: "https://i.ibb.co/KXjF6bg/nb-orange.png",
         price: 125
       },
       {
-        id: 11,
+        id: 12,
         name: "Champion Navy Woman",
         imageUrl: "https://i.ibb.co/rkyRdZq/champ-navy.png",
         price: 150
       },
       {
-        id: 12,
+        id: 13,
         name: "Adidas Grey",
         imageUrl: "https://i.ibb.co/fNYWWzh/adidas-grey.png",
         price: 110
       },
       {
-        id: 13,
+        id: 14,
         name: "Adidas Black Woman",
         imageUrl: "https://i.ibb.co/Nmb47Lb/adidas-black.png",
         price: 130
       },
       {
-        id: 14,
+        id: 15,
         name: "Nike Green",
         imageUrl: "https://i.ibb.co/vsknRzL/nike-green.png",
         price: 160
       },
       {
-        id: 15,
+        id: 16,
         name: "Nike Black/White",
         imageUrl: "https://i.ibb.co/G26LN37/nike-bw.png",
         price: 150
       },
       {
-        id: 16,
+        id: 17,
         name: "New Balance Red",
         imageUrl: "https://i.ibb.co/55c0X1N/nb-red.png",
         price: 190
       },
       {
-        id: 17,
+        id: 18,
         name: "Champion Navy Man",
         imageUrl: "https://i.ibb.co/86J7mmz/champion-navy.png",
         price: 160
       },
       {
-        id: 17,
+        id: 19,
         name: "Nike Grey",
         imageUrl: "https://i.ibb.co/99fTXBh/nike-grey.png",
         price: 130
       },
       {
-        id: 17,
+        id: 20,
         name: "Nike Black",
         imageUrl: "https://i.ibb.co/WktC5TN/nike-black.png",
         price: 140
@@ -133,61 +139,61 @@ const SHOP_DATA = {
     routeName: "tees",
     items: [
       {
-        id: 18,
+        id: 21,
         name: "Nike SB Green",
         imageUrl: "https://i.ibb.co/6mLxMyd/nike-sb.png",
         price: 60
       },
       {
-        id: 19,
+        id: 22,
         name: "Nike Blank",
         imageUrl: "https://i.ibb.co/2n2ST3p/nike-blank.png",
         price: 80
       },
       {
-        id: 20,
+        id: 23,
         name: "Nike Peach",
         imageUrl: "https://i.ibb.co/bdHVcxn/nike-peach.png",
         price: 50
       },
       {
-        id: 21,
+        id: 24,
         name: "Adidas Black",
         imageUrl: "https://i.ibb.co/r7Rhhg4/adidas-tee-black.png",
         price: 40
       },
       {
-        id: 22,
+        id: 25,
         name: "Nike SB Olive",
         imageUrl: "https://i.ibb.co/qkXGGD8/nike-sb-olive.png",
         price: 60
       },
       {
-        id: 22,
+        id: 26,
         name: "Adidas Purple",
         imageUrl: "https://i.ibb.co/HKSX1kP/adidas-purple.png",
         price: 50
       },
       {
-        id: 22,
+        id: 27,
         name: "New Balance White",
         imageUrl: "https://i.ibb.co/Tqn8jqq/nb-white.png",
         price: 40
       },
       {
-        id: 22,
+        id: 28,
         name: "Adidas Red",
         imageUrl: "https://i.ibb.co/QfFs6CL/adidas-red.png",
         price: 50
       },
       {
-        id: 22,
+        id: 29,
         name: "Nike ACG Pink",
         imageUrl: "https://i.ibb.co/G9F6fhq/nike-pink.png",
         price: 40
       },
       {
-        id: 22,
+        id: 30,
         name: "Nike SB White",
         imageUrl: "https://i.ibb.co/m0mzRrF/nike-sb-white.png",
         price: 60
@@ -200,61 +206,61 @@ const SHOP_DATA = {
     routeName: "womens",
     items: [
       {
-        id: 23,
+        id: 31,
         name: "Nike Air Max 1 Safari",
         imageUrl: "https://i.ibb.co/gZ5YY7s/air-max-safari.png",
         price: 180
       },
       {
-        id: 24,
+        id: 32,
         name: "Nike Air Max 1 Teal",
         imageUrl: "https://i.ibb.co/wcW8CmB/air-max-teal.png",
         price: 150
       },
       {
-        id: 25,
+        id: 33,
         name: "Vans Classic Black",
         imageUrl: "https://i.ibb.co/7QK7F5D/vans-classic-black.png",
         price: 50
       },
       {
-        id: 26,
+        id: 34,
         name: "Adidas Superstar White",
         imageUrl: "https://i.ibb.co/HNrtxdK/superstar-white.png",
         price: 120
       },
       {
-        id: 27,
+        id: 35,
         name: "Nike Air Force Black",
         imageUrl: "https://i.ibb.co/4FjpZ19/air-force-black.png",
         price: 150
       },
       {
-        id: 28,
+        id: 36,
         name: "Nike Air Jordan Pink",
         imageUrl: "https://i.ibb.co/VJRsxvt/air-jordan-pink.png",
         price: 200
       },
       {
-        id: 29,
+        id: 37,
         name: "Adidas NMD Special",
         imageUrl: "https://i.ibb.co/Rjh13hC/nmd-special.png",
         price: 250
       },
       {
-        id: 29,
+        id: 38,
         name: "Nike Air Max 90 Pink",
         imageUrl: "https://i.ibb.co/pJ6Cfgq/air-max-90-pink.png",
         price: 220
       },
       {
-        id: 29,
+        id: 39,
         name: "New Balance X90 Pink",
         imageUrl: "https://i.ibb.co/SNZ1g2j/nb-x90.png",
         price: 200
       },
       {
-        id: 29,
+        id: 40,
         name: "Adidas Yeezy Pink",
         imageUrl: "https://i.ibb.co/Bj5Sv7s/jeezy-pink.png",
         price: 450
@@ -267,61 +273,61 @@ const SHOP_DATA = {
     routeName: "mens",
     items: [
       {
-        id: 30,
+        id: 41,
         name: "Nike Janoski Camo",
         imageUrl: "https://i.ibb.co/86Y7sJD/janoski-camo.png",
         price: 150
       },
       {
-        id: 31,
+        id: 42,
         name: "Nike Air Presto Black",
         imageUrl: "https://i.ibb.co/9pZCQyC/air-presto-black.png",
         price: 220
       },
       {
-        id: 32,
+        id: 43,
         name: "Asics Gel Lyte Camo",
         imageUrl: "https://i.ibb.co/L124w6b/gel-lyte-camo.png",
         price: 125
       },
       {
-        id: 33,
+        id: 44,
         name: "Air Max 97 Gold",
         imageUrl: "https://i.ibb.co/pvPNcHf/air-max-97-gold.png",
         price: 350
       },
       {
-        id: 34,
+        id: 45,
         name: "Nike Janoski Pink",
         imageUrl: "https://i.ibb.co/vZppD3x/janoski-pink.png",
         price: 150
       },
       {
-        id: 35,
+        id: 46,
         name: "Adidas Superstar Black",
         imageUrl: "https://i.ibb.co/5vvrrBR/superstar-black.png",
         price: 135
       },
       {
-        id: 35,
+        id: 47,
         name: "Nike Janoski Bourdon",
         imageUrl: "https://i.ibb.co/qB064wg/janoski-bourdon.png",
         price: 125
       },
       {
-        id: 35,
+        id: 48,
         name: "Nike Air Force Yellow",
         imageUrl: "https://i.ibb.co/mFcjwwW/air-force-yellow.png",
         price: 190
       },
       {
-        id: 35,
+        id: 49,
         name: "Adidas NMD Black",
         imageUrl: "https://i.ibb.co/PcqvC37/nmd-black.png",
         price: 185
       },
       {
-        id: 35,
+        id: 50,
         name: "Air Max 90 Rainbow",
         imageUrl: "https://i.ibb.co/jwwt0BC/air-max-90-rainbow.png",
         price: 250
