846795e85308208062e5e72d0d6f284831fb1df4
bhajko <bhajko@gmail.com>
2019-10-19 15:46:24 +0200

    womans sneakers pics added

--- src/redux/shop/shop.data.js
+++ src/redux/shop/shop.data.js
@@ -159,45 +159,63 @@ const SHOP_DATA = {
     items: [
       {
         id: 23,
-        name: "Blue Tanktop",
-        imageUrl: "https://i.ibb.co/7CQVJNm/blue-tank.png",
-        price: 25
+        name: "Nike Air Max 1 Safari",
+        imageUrl: "https://i.ibb.co/gZ5YY7s/air-max-safari.png",
+        price: 180
       },
       {
         id: 24,
-        name: "Floral Blouse",
-        imageUrl: "https://i.ibb.co/4W2DGKm/floral-blouse.png",
-        price: 20
+        name: "Nike Air Max 1 Teal",
+        imageUrl: "https://i.ibb.co/wcW8CmB/air-max-teal.png",
+        price: 150
       },
       {
         id: 25,
-        name: "Floral Dress",
-        imageUrl: "https://i.ibb.co/KV18Ysr/floral-skirt.png",
-        price: 80
+        name: "Vans Classic Black",
+        imageUrl: "https://i.ibb.co/7QK7F5D/vans-classic-black.png",
+        price: 50
       },
       {
         id: 26,
-        name: "Red Dots Dress",
-        imageUrl: "https://i.ibb.co/N3BN1bh/red-polka-dot-dress.png",
-        price: 80
+        name: "Adidas Superstar White",
+        imageUrl: "https://i.ibb.co/HNrtxdK/superstar-white.png",
+        price: 120
       },
       {
         id: 27,
-        name: "Striped Sweater",
-        imageUrl: "https://i.ibb.co/KmSkMbH/striped-sweater.png",
-        price: 45
+        name: "Nike Air Force Black",
+        imageUrl: "https://i.ibb.co/4FjpZ19/air-force-black.png",
+        price: 150
       },
       {
         id: 28,
-        name: "Yellow Track Suit",
-        imageUrl: "https://i.ibb.co/v1cvwNf/yellow-track-suit.png",
-        price: 135
+        name: "Nike Air Jordan Pink",
+        imageUrl: "https://i.ibb.co/VJRsxvt/air-jordan-pink.png",
+        price: 200
       },
       {
         id: 29,
-        name: "White Blouse",
-        imageUrl: "https://i.ibb.co/qBcrsJg/white-vest.png",
-        price: 20
+        name: "Adidas NMD Special",
+        imageUrl: "https://i.ibb.co/Rjh13hC/nmd-special.png",
+        price: 250
+      },
+      {
+        id: 29,
+        name: "Nike Air Max 90 Pink",
+        imageUrl: "https://i.ibb.co/pJ6Cfgq/air-max-90-pink.png",
+        price: 220
+      },
+      {
+        id: 29,
+        name: "New Balance X90 Pink",
+        imageUrl: "https://i.ibb.co/SNZ1g2j/nb-x90.png",
+        price: 250
+      },
+      {
+        id: 29,
+        name: "Adidas Yeezy Pink",
+        imageUrl: "https://i.ibb.co/Bj5Sv7s/jeezy-pink.png",
+        price: 450
       }
     ]
   },
