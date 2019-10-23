e0be768f36ed16a1a8514901e76c9d5d7d4a8c56
bhajko <bhajko@gmail.com>
2019-10-23 13:02:53 +0200

    homepage scss

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -2,15 +2,30 @@ import React from "react";
 import Directory from "../../components/directory/directory";
 import Carousel from "../../components/carousel/carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { HomePageContainer } from "./homepage.styles";
+import "./homepage.scss";
 
 const HomePage = () => {
   return (
     <>
       <Carousel />
-      <HomePageContainer>
+      <div className="about">
+        <div className="about-text">
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam
+            aliquam possimus atque ad, quia dolor ex. Sint, ex hic. Dicta sed,
+            exercitationem ipsam obcaecati voluptatem ex illum eos nostrum quas.
+          </p>
+        </div>
+        <div className="about-pic">
+          <img
+            src="https://i.ibb.co/Jj0CVJK/air-max-red.png"
+            alt="air-max-1-red"
+          />
+        </div>
+      </div>
+      <div className="homepage">
         <Directory />
-      </HomePageContainer>
+      </div>
     </>
   );
 };
