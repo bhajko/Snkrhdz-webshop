c46c0fc6da13d324c389b70a994ab3e726a9bfe7
bhajko <bhajko@gmail.com>
2019-10-23 13:05:35 +0200

    homepage scss

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -8,21 +8,6 @@ const HomePage = () => {
   return (
     <>
       <Carousel />
-      <div className="about">
-        <div className="about-text">
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam
-            aliquam possimus atque ad, quia dolor ex. Sint, ex hic. Dicta sed,
-            exercitationem ipsam obcaecati voluptatem ex illum eos nostrum quas.
-          </p>
-        </div>
-        <div className="about-pic">
-          <img
-            src="https://i.ibb.co/Jj0CVJK/air-max-red.png"
-            alt="air-max-1-red"
-          />
-        </div>
-      </div>
       <div className="homepage">
         <Directory />
       </div>
