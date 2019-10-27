3cc83cae365cf5dc299ef7052ab540fa7d1bd804
bhajko <bhajko@gmail.com>
2019-10-27 19:22:02 +0100

    about-us design added

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import "./about.scss";
 
 const AboutPage = () => {
   return (
-    <div>
-      <h1>About us</h1>
+    <div className="about-container">
+      <div className="about-pic"></div>
+      <div className="about-us">
+        <div className="about-text">
+          <h3>About us</h3>
+          <p>
+            We work exclusively with trusted manufacturers guaranteeing the
+            latest range, including limited must-havy. The selection in our
+            store is very subjective, based on timeless designs, functionality,
+            natural materials and original solutions.
+          </p>
+          <br />
+          <p>
+            We do not rest on our laurels, so we still expanding with new makes
+            and models, so that the displayed products always reflect the world
+            stage.
+          </p>
+        </div>
+        <div className="about-map">map</div>
+      </div>
     </div>
   );
 };
